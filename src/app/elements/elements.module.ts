5542dda7b977d3d54caf29d808af9cb65dac444e
carlesnacher <carlesnacher@gmail.com>
2022-11-23 12:39:31 +0100

    Agrego el componente home

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { CharactersComponent } from './characters/characters.component';
+import { HomeComponent } from './home/home.component';
 
 
 
@@ -10,7 +11,8 @@ import { CharactersComponent } from './characters/characters.component';
   declarations: [
     NavbarComponent,
     AboutComponent,
-    CharactersComponent
+    CharactersComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
