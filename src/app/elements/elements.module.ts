256906c722af2dffa2eb8b7e69dc23eb6324ea22
carlesnacher <carlesnacher@gmail.com>
2022-11-23 12:26:35 +0100

    Agrego el componente characters

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
+import { CharactersComponent } from './characters/characters.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    AboutComponent
+    AboutComponent,
+    CharactersComponent
   ],
   imports: [
     CommonModule
